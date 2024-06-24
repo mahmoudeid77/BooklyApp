08cf5116f588581315eaa9047f51a213cf0d39c0
mahmoudeid77 <mahmoudmohamedeid77@gmail.com>
2024-06-24 06:49:52 +0300

    using lottie package

---
+++ lib/core/utilis/costants.dart
@@ -0,0 +1,5 @@
+
+
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 64, 44, 130);
--- lib/features/presentation/views/widgets/splash_view_body.dart
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_application_1/core/utilis/assets.dart';
+import 'package:lottie/lottie.dart';
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Scaffold(
+      body: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(AssetsData.logo),
+        Lottie.network('https://lottie.host/17185415-54e0-4521-89b2-f3b01fee4834/kbRR1ufXvw.json')
+      ],
+    ),
+    );
   }
 }
