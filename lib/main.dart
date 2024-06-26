1502ee9c4fb6859c783f187ad27325d77d1041d4
mahmoudeid77 <mahmoudmohamedeid77@gmail.com>
2024-06-26 03:29:22 +0300

    using google fonts package

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/core/utilis/costants.dart';
 import 'package:flutter_application_1/features/presentation/views/splash_view.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -17,7 +18,10 @@ class BooklyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
             scaffoldBackgroundColor: kPrimaryColor,
+              textTheme: GoogleFonts.montserratAlternatesTextTheme(ThemeData.dark().textTheme),
           ),
+   
+          
       home: SplashView(),
     );
   }
