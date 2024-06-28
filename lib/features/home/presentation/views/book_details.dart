863b8929b75cdba90ea411024a8be035f0d28254
mahmoudeid77 <mahmoudmohamedeid77@gmail.com>
2024-06-28 11:55:42 +0300

    books action done

--- lib/features/home/presentation/views/book_details.dart
+++ lib/features/home/presentation/views/book_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/core/utilis/styles.dart';
 import 'package:flutter_application_1/features/home/presentation/widgets/book_cover.dart';
+import 'package:flutter_application_1/features/home/presentation/widgets/books_action.dart';
 import 'package:flutter_application_1/features/home/presentation/widgets/custom_appbar_book_details.dart';
 import 'package:flutter_application_1/features/home/presentation/widgets/second_evaluation_row.dart';
 
@@ -21,7 +22,9 @@ class BookDetails extends StatelessWidget {
             SizedBox(height: 5,),
             Text('by Markus Zustin',style: AppStyles.textStyle20,),
             SizedBox(height: 20,),
-            Center(child: SecondEvaluationRow())
+            Center(child: SecondEvaluationRow()),
+            SizedBox(height: 20,),
+            BooksAction(),
           ],
         ),
       ),
---
+++ lib/features/home/presentation/widgets/books_action.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/widgets/custom_button.dart';
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        children: [
+          const Expanded(
+              child: CustomButton(
+            text: 'Free',
+            backgroundColor: Colors.white,
+            textColor: Colors.black,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16),
+              bottomLeft: Radius.circular(16),
+            ),
+          )),
+          Expanded(
+              child: CustomButton(
+            onPressed: () {
+             
+            },
+            fontSize: 16,
+            text: '250\$',
+            backgroundColor: const Color(0xffEF8262),
+            textColor: Colors.white,
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(16),
+              bottomRight: Radius.circular(16),
+            ),
+          )),
+        ],
+      ),
+    );
+  }
+}
