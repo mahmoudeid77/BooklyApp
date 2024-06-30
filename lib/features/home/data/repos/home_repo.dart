ae524aab17f48ca193b7048a120987f1471ebbf7
mahmoudeid77 <mahmoudmohamedeid77@gmail.com>
2024-06-30 11:21:35 +0300

    create home repo

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_application_1/errors/failure.dart';
+import 'package:flutter_application_1/features/home/data/book_model/book_model.dart';
+
 abstract class HomeRepo {
-  FetchBestSellerBooks();
-  FetchFeaturedBooks();
+  Future <Either<Failure,List<BookModel>>> FetchBestSellerBooks();
+    Future <Either<Failure,List<BookModel>>> FetchFeaturedBooks();
 }
